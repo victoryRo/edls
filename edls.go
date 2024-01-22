b93d456c4d7dd6f7e9cf5e164137209ef4d58ce3
Victor <yosoy@Victors-iMac.local>
2024-01-22 09:05:20 -0500

    Declaration of constants for file types

---
+++ edls.go
@@ -0,0 +1,24 @@
+package main
+
+// file types
+const (
+	regular int = iota
+	directory
+	executable
+	compress
+	image
+	link
+)
+
+// file extension
+const (
+	exe = ".exe"
+	deb = ".deb"
+	zip = ".zip"
+	gz  = ".gz"
+	tar = ".tar"
+	rar = ".rar"
+	png = ".png"
+	jpg = ".jpg"
+	gif = ".gif"
+)
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/victoryRo/edls
+
+go 1.21.5
